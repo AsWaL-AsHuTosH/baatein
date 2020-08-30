ede5c339a3612dcc3c9a38360cd82fa5cb604f6c
AsWaL-AsHuTosH <ashu.aswal.333@gmail.com>
2020-08-30 22:10:28 +0530

    new

--- lib/login_reg/signin_screen.dart
+++ lib/login_reg/signin_screen.dart
@@ -8,6 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:baatein/chat/home_screen.dart';
 import 'package:baatein/constants/constants.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SignInScreen extends StatefulWidget {
   static const routeId = 'log_in_screen';
@@ -23,155 +24,172 @@ class _SignInScreenState extends State<SignInScreen> {
   bool otherEmailError = false;
   bool otherPasswordError = false;
   String errorMessage;
+  bool checkingLastUser = true;
   @override
   void initState() {
     super.initState();
-    Firebase.initializeApp();
+    checkLastUser();
+  }
+
+  void checkLastUser() async {
+    await Firebase.initializeApp();
+    setState(() {
+      checkingLastUser = false;
+    });
+    if (FirebaseAuth.instance.currentUser != null) {
+      Navigator.pushNamed(context, HomeScreen.routeId);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: ModalProgressHUD(
-        inAsyncCall: spin,
-        child: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                stops: [0.1, 0.5, 0.7, 0.9],
-                colors: [
-                  Colors.red[800],
-                  Colors.yellow[700],
-                  Colors.yellow[600],
-                  Colors.yellow[400],
-                ],
+    return checkingLastUser
+        ? SpinKitFadingCircle(
+            color: Colors.white,
+            size: 50.0,
+          )
+        : Scaffold(
+            backgroundColor: Theme.of(context).primaryColor,
+            body: ModalProgressHUD(
+              inAsyncCall: spin,
+              child: SafeArea(
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      stops: [0.1, 0.5, 0.7, 0.9],
+                      colors: [
+                        Colors.red[800],
+                        Colors.yellow[700],
+                        Colors.yellow[600],
+                        Colors.yellow[400],
+                      ],
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: Text(
+                          'Baatein',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                            fontFamily: 'DancingScript',
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(height: 10),
+                                ElivatedForm(
+                                  emailController: emailController,
+                                  passwordController: passwordController,
+                                  emailValidationCallback: (val) {
+                                    if (otherEmailError) {
+                                      otherEmailError = false;
+                                      return errorMessage;
+                                    }
+                                    return RegExp(
+                                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                            .hasMatch(val)
+                                        ? null
+                                        : "Please enter a valid email address!";
+                                  },
+                                  passwordValidationCallback: (val) {
+                                    if (otherPasswordError) {
+                                      otherPasswordError = false;
+                                      return errorMessage;
+                                    }
+                                    return val.length < 6
+                                        ? "Password should be at least 6 characters long!"
+                                        : null;
+                                  },
+                                  formKey: _formKey,
+                                ),
+                                RoundTextButton(
+                                  text: 'Sign In',
+                                  icon: Icons.verified_user,
+                                  margin: 60,
+                                  color: Theme.of(context).primaryColor,
+                                  onPress: () async {
+                                    setState(() {
+                                      spin = true;
+                                    });
+                                    if (!_formKey.currentState.validate()) {
+                                      setState(() {
+                                        spin = false;
+                                      });
+                                      return;
+                                    }
+                                    try {
+                                      await FirebaseAuth.instance
+                                          .signInWithEmailAndPassword(
+                                        email: emailController.text,
+                                        password: passwordController.text,
+                                      );
+                                      setState(() {
+                                        spin = false;
+                                      });
+                                      emailController.clear();
+                                      passwordController.clear();
+                                      Navigator.pushNamed(
+                                          context, HomeScreen.routeId);
+                                    } catch (e) {
+                                      if (e.toString() == kInvalidUser) {
+                                        otherEmailError = true;
+                                        errorMessage = kInvalidUserWarning;
+                                      } else if (e.toString() ==
+                                          kWrongPassword) {
+                                        otherPasswordError = true;
+                                        errorMessage = kWrongPasswordWarning;
+                                      } else
+                                        assert(false);
+                                      setState(() {
+                                        spin = false;
+                                      });
+                                      print('lest go');
+                                      _formKey.currentState.validate();
+                                    }
+                                  },
+                                ),
+                                Divider(
+                                  color: Colors.transparent,
+                                ),
+                                RoundTextButton(
+                                  text: 'Create New Account',
+                                  icon: Icons.person_add,
+                                  color: Colors.green,
+                                  margin: 20,
+                                  onPress: () => Navigator.pushNamed(
+                                      context, SignUpScreen.routeId),
+                                ),
+                              ],
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(150)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 10),
-                  child: Text(
-                    'Baatein',
-                    style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white,
-                      fontFamily: 'DancingScript',
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 10),
-                          ElivatedForm(
-                            emailController: emailController,
-                            passwordController: passwordController,
-                            emailValidationCallback: (val) {
-                              if (otherEmailError) {
-                                otherEmailError = false;
-                                return errorMessage;
-                              }
-                              return RegExp(
-                                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                      .hasMatch(val)
-                                  ? null
-                                  : "Please enter a valid email address!";
-                            },
-                            passwordValidationCallback: (val) {
-                              if (otherPasswordError) {
-                                otherPasswordError = false;
-                                return errorMessage;
-                              }
-                              return val.length < 6
-                                  ? "Password should be at least 6 characters long!"
-                                  : null;
-                            },
-                            formKey: _formKey,
-                          ),
-                          RoundTextButton(
-                            text: 'Sign In',
-                            icon: Icons.verified_user,
-                            margin: 60,
-                            color: Theme.of(context).primaryColor,
-                            onPress: () async {
-                              setState(() {
-                                spin = true;
-                              });
-                              if (!_formKey.currentState.validate()) {
-                                setState(() {
-                                  spin = false;
-                                });
-                                return;
-                              }
-                              try {
-                                await FirebaseAuth.instance
-                                    .signInWithEmailAndPassword(
-                                  email: emailController.text,
-                                  password: passwordController.text,
-                                );
-                                setState(() {
-                                  spin = false;
-                                });
-                                emailController.clear();
-                                passwordController.clear();
-                                Navigator.pushNamed(
-                                    context, HomeScreen.routeId);
-                              } catch (e) {
-                                if (e.toString() == kInvalidUser) {
-                                  otherEmailError = true;
-                                  errorMessage = kInvalidUserWarning;
-                                } else if (e.toString() == kWrongPassword) {
-                                  otherPasswordError = true;
-                                  errorMessage = kWrongPasswordWarning;
-                                } else
-                                  assert(false);
-                                setState(() {
-                                  spin = false;
-                                });
-                                print('lest go');
-                                _formKey.currentState.validate();
-                              }
-                            },
-                          ),
-                          Divider(
-                            color: Colors.transparent,
-                          ),
-                          RoundTextButton(
-                            text: 'Create New Account',
-                            icon: Icons.person_add,
-                            color: Colors.green,
-                            margin: 20,
-                            onPress: () => Navigator.pushNamed(
-                                context, SignUpScreen.routeId),
-                          ),
-                        ],
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius:
-                          BorderRadius.only(topLeft: Radius.circular(150)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
