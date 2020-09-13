70ad17a6a365f210b331eb3fb9f22b5eae111058
AsWaL-AsHuTosH <ashu.aswal.333@gmail.com>
2020-09-13 18:59:15 +0530

    commit

--- lib/chat/forward_selection_screen.dart
+++ lib/chat/forward_selection_screen.dart
@@ -3,7 +3,6 @@ import 'package:baatein/chat/group_forward_screen.dart';
 import 'package:baatein/helper/message_info.dart';
 import 'package:flutter/material.dart';
 
-
 class ForwardSelectionScreen extends StatefulWidget {
   final List<MessageInfo> selectedMessage;
   final String from;
@@ -26,11 +25,18 @@ class _ForwardSelectionScreenState extends State<ForwardSelectionScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Forward message'),
-        bottom: TabBar(controller: _tabController, tabs: [
-          Text('Chats'),
-          Text('Groups'),
-        ]),
+        title: Padding(
+          padding: const EdgeInsets.only(left: 30.0),
+          child: Text('Forward message', style: TextStyle(fontSize: 20),),
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: true,
+          tabs: [
+            Tab(text: 'Chats'),
+            Tab(text: 'Groups'),
+          ],
+        ),
       ),
       body: TabBarView(
         controller: _tabController,
